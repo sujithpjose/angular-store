7c01259f6d39b15c15850b9c0d87273ae8371a64
sujithpjs <sujith.jose@kongsbergdigital.com>
2024-06-24 19:05:01 +0530

    chore: graph,lint,test,e2e configured

--- apps/angular-store/src/app/app.routes.ts
+++ apps/angular-store/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+    {
+        path: 'products',
+        loadComponent: () =>
+            import('@angular-monorepo/products').then((m) => m.ProductsComponent),
+    },
+    {
+        path: 'orders',
+        loadComponent: () =>
+            import('@angular-monorepo/orders').then((m) => m.OrdersComponent),
+    },
+];
--- apps/inventory/src/app/app.component.ts
+++ apps/inventory/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcomeComponent } from './nx-welcome.component';
+import { ProductsComponent } from '@angular-monorepo/products';
 
 @Component({
   standalone: true,
-  imports: [NxWelcomeComponent, RouterModule],
+  imports: [ProductsComponent, RouterModule],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
